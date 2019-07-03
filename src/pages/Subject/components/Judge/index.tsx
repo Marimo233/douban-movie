7e818f938592dbccde21cbcc9942de82b25c2cec
marimo <1072452597@qq.com>
2019-07-03 20:15:15 +0800

    feat

--- src/pages/Subject/components/Judge/index.tsx
+++ src/pages/Subject/components/Judge/index.tsx
@@ -7,6 +7,9 @@ export default function Judge() {
   const desc=['很差', '较差','还行','推荐','力荐']
   return (
     <div className="judge-wrap">
+      <div>
+        
+      </div>
     <button>想看</button>
     <button>看过</button>
     <span>
